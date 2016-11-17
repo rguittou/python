f89a1a37fa79cf93dadccd7067a01d0be964b133
stagiaire <rabahguittoune@gmail.com>
2016-11-17 16:11:41 +0100

    "Flask script"

--- basic/web.py
+++ basic/web.py
@@ -1,15 +1,21 @@
 #!/usr/bib/python3
 
-
-from flask import Flask
+from flask import Flask,redirect,url_for
 app = Flask(__name__)
-
 @app.route("/")
-def hello():
-    return "Hello World!"
 
+def index():
+   return redirect(url_for('test'))
+@app.route("/test")
+
+def test():
+   return "Hello World!\n"
+
+@app.route('/user/<name>')
+def user(name):
+    return "<h1> Hello %s</h1>" % name
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
 
